ba82a8e139e5ac599d8122039d5932c88894b075
madonna-mosaad <madonnamosaad86@gmail.com>
2024-08-01 20:50:06 +0300

    part01

---
+++ Advanced03/BookFunctionsPointer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced03
+{
+    internal delegate string BookFunctionsPointer(Book B);
+   
+}
--- Advanced03/Program.cs
+++ Advanced03/Program.cs
@@ -5,7 +5,17 @@
         
         static void Main(string[] args)
         {
-            
+            #region part1
+            //List<Book> books = new List<Book> { new Book("1", "Math", new string[] { "Aya", "Sara" }, DateTime.Now, 100), new Book("2", "Science", new string[] { "Madonna", "Louji" }, DateTime.Now, 200) };
+            //BookFunctionsPointer bookFunctionsPointer = BookFunctions.Getauthors;
+            //LibraryEngine.ProcessBooks(books, bookFunctionsPointer);
+
+            //BookFunctionsPointer functionsPointer1 = delegate (Book b) { return b.ISBN; };
+            //BookFunctionsPointer functionsPointer2 = (Book b) => $"{b.PublicationDate}";
+            //LibraryEngine.ProcessBooks(books, functionsPointer1);
+            //LibraryEngine.ProcessBooks(books, functionsPointer2); 
+            #endregion
+
         }
     }
 }
